d24c2291e8cdb1cf8e839517f6f0a81d8d85adc1
WillyPChem <willypchem@gmail.com>
2017-02-15 16:18:38 -0500

    Building partially correct H matrix

--- Hamiltonian_Matrix.c
+++ Hamiltonian_Matrix.c
@@ -4,16 +4,28 @@
 
 int main() {
 
-int i, n;  
-double H [5] [5];
+int i, j,  n;  
+double H[5][5];
 
 for (i=0; i<5; i++) {
-  
+ 
   n = i + 1;
-  H[i][i] = -13.6/(n*n);
+  H[i][i] = -13.6/(pow(n , 2));
+
+  for (j=0; j<5; j++) {
+    if (i!=j) H[i][j] = 0.;
+  }
 }
 
-  
+for (i=0; i<5; i++) {
+
+  for (j=0; j<5; j++) {
+
+    printf("  %f  ",H[i][j]);
+
+  }
+  printf("\n");
+} 
   
 }
 
