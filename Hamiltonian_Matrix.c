2279ff081d18502c543ba27b0da24c96ab3ca671
WillyPChem <willypchem@gmail.com>
2017-02-15 16:02:27 -0500

    Update Hamiltonian_Matrix.c

--- Hamiltonian_Matrix.c
+++ Hamiltonian_Matrix.c
@@ -1,13 +1,19 @@
-int twodim [x] [y]
-int H [5] [5]={
-{1,2,3,4,5},
-{6,7,8,9,10},
-{11,12,13,14,15},
-{16,17,18,19,20},
-{21,22,23,24,25},
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
-int H [1] [1]
-En= -13.60/n^2
-n=
-};
+int main() {
+
+int i, n;  
+double H [5] [5];
+
+for (i=0; i<5; i++) {
+  
+  n = i + 1;
+  H[i][i] = -13.6/(n*n);
+}
+
+  
+  
+}
 
