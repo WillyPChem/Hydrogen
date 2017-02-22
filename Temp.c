60841fd9daab257c273796d7ef2caccd28f1e740
bryn94 <andersonb6@student.wpunj.edu>
2017-02-22 15:28:02 -0500

    Create Temp.c

---
+++ Temp.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main() {
+ int i, n, l, q, m;
+ int j, Norbs;
+int B[30][3];
+
+for (
+
+
+
+
+for (i=0; i<30; i++) {
+for (n=1; n
+ Norbs(n)=n*n;
+ i=Norbs+1
+ B[Norbs][0]=1
+ E=
+  H[i][i] = -13.6/(pow(n , 2));
+
+  for (j=0; j<5; j++) {
+    if (i!=j) H[i][j] = 0.;
+  }
+}
+
+for (i=0; i<5; i++) {
+
+  for (j=0; j<5; j++) {
+
+    printf("  %f  ",H[i][j]);
+
+  }
+  printf("\n");
+} 
+  
+}
