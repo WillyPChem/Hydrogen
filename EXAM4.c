c184816a2d4ee1fe4b5d7297535184a3d1c243a4
bryn94 <andersonb6@student.wpunj.edu>
2017-03-01 15:26:51 -0500

    Update EXAM4.c

--- EXAM4.c
+++ EXAM4.c
@@ -1,35 +1,32 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-
+#include<
 
 int main()    {
 
-int i, MAX_I; 
-double x, fxdx, fxpdx, dx, fx, xpdx, fp, m;
-double  fx2 = fx*fx;
-double pi;
+int i, MAX_r; 
+double r;
+double fRdr, dr, fR;
+double e;
+e = 1
 
 
-pi = 4.*atan(1.0);
-
-MAX_I = 1000;
-dx = 10./MAX_I;
+MAX_r = 100;
+dr = 10./MAX_r;
 
 
 
 
-for (i=0; i<=MAX_I; i++) {
+for (i=0; i<=MAX_r; i++) {
+ 
+   r = dr*i;
 
-   x = dx*i;
-   xpdx = x + dx;
    
-   fx = sqrt(2./10.)*sin(pi*x/10.);
-   fp = sqrt(2./10.)*(pi/10.)*cos(pi*x/10.);
-   fxpdx = sqrt(2./10)*sin(pi*xpdx/10.);
-   m = (fxpdx-fx)/(xpdx-x);
+   fr = R10 * e * r * R20;
+   
 
-  printf(" %i %f  %f %f  \n", i, x, fp, m);
+  printf(" %i %f  %f %f  \n", r, R, fr, );
 }
 } 
  
