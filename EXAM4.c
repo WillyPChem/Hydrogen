43a48b256678739374b782101ed490442dccdd5d
bryn94 <andersonb6@student.wpunj.edu>
2017-03-01 16:06:28 -0500

    Update EXAM4.c

--- EXAM4.c
+++ EXAM4.c
@@ -1,14 +1,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#include<
+#include<Radial.h>
 
 int main()    {
 
 int i, MAX_r; 
-double r;
-double fRdr, dr, fR;
+double r, j;
+double frdr, dr, fr, sum;
 double e;
+
 e = 1
 
 
@@ -16,15 +17,17 @@ MAX_r = 100;
 dr = 10./MAX_r;
 
 
-
-
+ sum = 0.;
 for (i=0; i<=MAX_r; i++) {
- 
-   r = dr*i;
+ r = dr*i;
+ double R10 = Radial_Orb(1,0,r);
+ double R20 = Radial_Orb(2,0,r);
 
    
-   fr = R10 * e * r * R20;
+ 
    
+   fr = R10 * e * r * r* r * R20;
+   sum = sum + fr * dr
 
   printf(" %i %f  %f %f  \n", r, R, fr, );
 }
