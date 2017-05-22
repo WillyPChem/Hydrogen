0d07c863039dfe3cabde02a55faba6b999ea7535
WillyPChem <willypchem@gmail.com>
2017-05-22 10:33:50 -0400

    Update Hydrogen_Simulator.c

--- Hydrogen_Simulator.c
+++ Hydrogen_Simulator.c
@@ -1,3 +1,26 @@
+/*
+  Hydrogen_Simulator.c - A simulator for a hydrogen atom interacting with an intense electromagnetic field using the time-dependent
+  configuration interaction method to solve the time-dependent configuration interaction method.
+  
+    Copyright (C) 2017  Jonathan J. Foley IV, Henrietta Eugene, Brymor Anderson, 
+    Rana Daher, Laura Perdomo-Quintero, Oyewumi Akinfenwa, Muttalib Khan.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Electronic Contact:  foleyj10@wpunj.edu
+    Mail Contact:   Prof. Jonathan Foley
+                    Department of Chemistry, William Paterson University
+                    300 Pompton Road
+                    Wayne NJ 07470
+*/
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
